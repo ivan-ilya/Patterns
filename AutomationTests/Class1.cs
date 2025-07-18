ba5a6bc882d018fa1e9d332db2db9805e40ec1e2
i.bondarenko <i.bondarenko@avatrade.com>
2025-07-18 19:35:07 +0400

    test pipeline

---
+++ AutomationTests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace AutomationTests;
+
+public class Class1
+{
+    [Test]
+    public void AdditionTest()
+    {
+        Assert.That(2 + 2, Is.EqualTo(4));
+    }
+}
